adfd9a9531ff38559f0798ef4f766413a4987b57
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-07-22 20:21:50 +0900

    feat: 나의 티켓, 판매 티켓 구현

--- frontend/src/pages/MyPage.js
+++ frontend/src/pages/MyPage.js
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Grid } from "@mui/material";
 import Kdy from "../images/kdy.jpeg";
 import ether from "../images/ethereum.png";
-
 const ConnectedContainer = styled.div`
   display: flex;
   align-items: center;
@@ -33,6 +32,7 @@ const ConnectedContainer = styled.div`
     border-radius: 50%;
     object-fit: cover;
     background: grey;
+    cursor: pointer;
   }
 
   & .dappAddressWrapper {
@@ -64,52 +64,140 @@ const UserInfo = styled.div`
   margin-top: 50px;
 `;
 
+const MyPageContainer = styled.div`
+  padding-left: 5%;
+  padding-right: 5%;
+  height: 500px;
+  border-bottom: 2px solid #e9e9f0;
+
+  & > h2 {
+    margin-top: 20px;
+    padding-left: 4%;
+  }
+
+  & > div {
+    display: grid;
+    justify-items: center;
+    grid-template-columns: repeat(auto-fill, minmax(20%, auto));
+  }
+
+  & > div > div {
+    width: 300px;
+    height: 400px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 30px;
+  }
+
+  & .myPage_ticket_image {
+    width: 200px;
+    height: 300px;
+  }
+
+  & .myPage_ticket_date {
+    width: 200px;
+    font-weight: 700;
+    margin-top: 15px;
+  }
+
+  & .myPage_ticket_desc {
+    width: 200px;
+    margin-top: 15px;
+  }
+`;
 const MyPage = () => {
+  const [dummy, setDummy] = useState([1, 2, 3, 4, 5]);
+
   return (
-    <ConnectedContainer>
-      {/* 배경 */}
-      <img
-        className="backgroundImg"
-        src={`https://image.kmib.co.kr/online_image/2022/0101/2021123118131057409_1640941990_0924225097.jpg`}
-        alt=""
-      />
-      <Grid container spacing={2}>
-        <Grid item xs={4}>
-          <div className="profileImgLocation" style={{}}>
-            <img
-              className="profileSize"
-              src={Kdy}
-              onError={({ currentTarget }) => {
-                currentTarget.onerror = null; // prevents looping
-                currentTarget.src = "images/MetaMask_Fox.svg.png";
-              }}
-              alt=""
-            />
-          </div>
+    <>
+      <ConnectedContainer>
+        {/* 배경 */}
+        <img
+          className="backgroundImg"
+          src={`https://image.kmib.co.kr/online_image/2022/0101/2021123118131057409_1640941990_0924225097.jpg`}
+          alt=""
+        />
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <div className="profileImgLocation" style={{}}>
+              <img
+                className="profileSize"
+                src={Kdy}
+                onError={({ currentTarget }) => {
+                  currentTarget.onerror = null; // prevents looping
+                  currentTarget.src = "images/MetaMask_Fox.svg.png";
+                }}
+                alt=""
+              />
+            </div>
+          </Grid>
         </Grid>
-      </Grid>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: "0.5rem",
-        }}
-      >
-        {/* 닉네임 */}
-        <UserInfo>
-          <h1>김동영</h1>
-        </UserInfo>
-        {/* 지갑 주소 */}
-        <div className="dappAddressWrapper">
-          <img className="etherSize" src={ether} alt="eth" />
-          <div className="dappAddressLength">
-            {window.ethereum.selectedAddress}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "0.5rem",
+          }}
+        >
+          {/* 닉네임 */}
+          <UserInfo>
+            <h1>김동영</h1>
+          </UserInfo>
+          {/* 지갑 주소 */}
+          <div className="dappAddressWrapper">
+            <img className="etherSize" src={ether} alt="eth" />
+            <div className="dappAddressLength">
+              {window.ethereum.selectedAddress}
+            </div>
           </div>
         </div>
-      </div>
-    </ConnectedContainer>
+      </ConnectedContainer>
+      <>
+        <MyPageContainer>
+          <h2>나의 티켓</h2>
+          <div>
+            {dummy.map((el) => {
+              return (
+                <div>
+                  <img
+                    className="myPage_ticket_image"
+                    alt=""
+                    src={`http://ticketimage.interpark.com/TCMS4/Main/201903/TicketTodayNew_TicketTodayDrama_5c9237a5-782b-4c0d-8beb-2e70ebe260a0.jpg`}
+                  ></img>
+                  <div className="myPage_ticket_date">2022.07.16</div>
+                  <div className="myPage_ticket_desc">
+                    2022 10년 연속 1위 연극 옥탑방고양이-틴틴홀
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </MyPageContainer>
+        <MyPageContainer>
+          <h2>판매 티켓</h2>
+          <div>
+            {dummy.map((el) => {
+              return (
+                <div>
+                  <img
+                    className="myPage_ticket_image"
+                    alt=""
+                    src={`http://ticketimage.interpark.com/TCMS4/Main/201903/TicketTodayNew_TicketTodayDrama_5c9237a5-782b-4c0d-8beb-2e70ebe260a0.jpg`}
+                  ></img>
+                  <div className="myPage_ticket_date">2022.07.16</div>
+                  <div className="myPage_ticket_desc">
+                    2022 10년 연속 1위 연극 옥탑방고양이-틴틴홀
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </MyPageContainer>
+      </>
+    </>
   );
 };
 
