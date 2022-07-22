ecdc8c4a94f864a17f9b5f6f546a93fd28ac9ccc
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-07-22 11:10:22 +0900

    feat: banner5 (피드 배너) 구현

---
+++ frontend/src/components/Home/Banner5.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner5 = () => {
+  return (
+    <div style={{ marginLeft: "10%" }} className="SliderWrapper">
+      <div className="textWrapper">
+        <div class="test">최고의</div>
+        <div style={{ marginTop: "15px" }} class="test">
+          <span>커뮤니케이션 공간 </span>
+        </div>
+        <div style={{ marginTop: "15px" }} class="test cl">
+          피드
+        </div>
+        <div className="disc">
+          인플루언서의 팬덤과 소통할 수 있는 공간입니다.
+        </div>
+        <div className="disc2">
+          물론, 인플루언서의 게시글도 확인할 수 있습니다.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner5;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,6 +4,7 @@ import Banner1 from "../components/Home/Banner1";
 import Banner2 from "../components/Home/Banner2";
 import Banner3 from "../components/Home/Banner3";
 import Banner4 from "../components/Home/Banner4";
+import Banner5 from "../components/Home/Banner5";
 const HomeBox = styled.div`
   display: flex;
   flex-direction: column;
@@ -16,6 +17,7 @@ const Home = () => {
       <Banner2></Banner2>
       <Banner3></Banner3>
       <Banner4></Banner4>
+      <Banner5></Banner5>
     </HomeBox>
   );
 };
