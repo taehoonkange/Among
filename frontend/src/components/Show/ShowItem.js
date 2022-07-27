c69f1d5e6c861d81b5e2264b930692bce9d9cc76
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-07-27 23:32:26 +0900

    feat: 공연이미지 구현

--- frontend/src/components/Show/ShowItem.js
+++ frontend/src/components/Show/ShowItem.js
@@ -1,7 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+const PerformContainer = styled.div`
+  width: 260px;
+`;
+
+const PosterImgContainer = styled.img`
+  width: 260px;
+  height: 260px;
+  &:hover {
+    transform: scale(1.03);
+  }
+  // boder-radius: "15px";
+  object-fit: cover;
+`;
+
+const handleError = (e) => {
+  e.target.src = "images/posterImg1.png";
+};
 
 const ShowItem = () => {
-  return <div>ShowItem</div>;
+  return (
+    <PerformContainer>
+      <PosterImgContainer
+        src={`https://ticketimage.interpark.com/Play/image/large/22/22008692_p.gif`}
+        onError={handleError}
+        alt="poster img"
+      />
+    </PerformContainer>
+  );
 };
 
 export default ShowItem;
