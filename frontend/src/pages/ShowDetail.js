bfad737e2e40456da5f167d416214be7c8446196
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-07-28 00:22:44 +0900

    feat: 공연세부정보 라우트 설정, 페이지 생성

---
+++ frontend/src/pages/ShowDetail.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const TopCss = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+`;
+
+const ShowDetail = () => {
+  return <TopCss></TopCss>;
+};
+
+export default ShowDetail;
