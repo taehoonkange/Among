d618acce104a7babe3b14323e3ebb545808575e6
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-07-28 14:41:32 +0900

    feat: date-picker 한글 적용

--- frontend/src/components/ShowDetail/DatePick/DatePick.js
+++ frontend/src/components/ShowDetail/DatePick/DatePick.js
@@ -2,11 +2,18 @@ import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./DatePick.css";
+import { ko } from "date-fns/esm/locale";
+
 const DatePick = () => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(null);
   return (
-    <DatePicker startDate={startDate} endDate={endDate} inline></DatePicker>
+    <DatePicker
+      startDate={startDate}
+      endDate={endDate}
+      inline
+      locale={ko}
+    ></DatePicker>
   );
 };
 
