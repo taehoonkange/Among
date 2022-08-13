6205d413ec4d69338bd545079025f8bb0b73a5a6
taehoonkang <sls97117@gmail.com>
2022-08-13 14:03:58 +0900

    feat: user model orm

---
+++ packages/backend/models/user.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define(`User`, {
+        wallet_address: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        nickname: {
+            type: DataTypes.STRING(15)
+        },
+        user_type: {
+                type: DataTypes.ENUM({
+                        values: [`NORMAL`, `VIP`, `VVIP`]
+                    }
+                ),
+                defaultValue: `NORMAL`,
+                allowNull: false
+            },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        img_src: {
+            type: DataTypes.STRING(200),
+            allowNull: true
+        }
+    }, {
+        modelName: 'User',
+        tableName: 'users',
+        charset: `utf8`,
+        collate: `utf8_general_ci`
+    });
+    User.associate = (db) => {
+        db.User.hasMany(db.Ticket);
+        db.User.hasMany(db.Performance);
+        db.User.hasMany(db.Comment);
+        db.User.hasMany(db.Board);
+
+    }
+
+    return User;
+}
