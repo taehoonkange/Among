84e4a91c9d20ad31662c854733c7743d8f061617
taehoonkang <sls97117@gmail.com>
2022-08-13 14:05:27 +0900

    fix: orm 추가로 index 수정

--- packages/backend/models/index.js
+++ packages/backend/models/index.js
@@ -1,9 +1,16 @@
 const Sequelize = require(`Sequelize`)
 const env = process.env.NODE_ENV || `development`;
-const config = require(`config/config`)[env]
+const config = require(`../config/config`)[env]
 const db= {}
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config)
+
+db.Performance = require(`./performance`)(sequelize,Sequelize)
+db.User = require(`./user`)(sequelize,Sequelize)
+db.Seat = require(`./seat`)(sequelize,Sequelize)
+db.Ticket = require(`./ticket`)(sequelize,Sequelize)
+db.Comment = require(`./comment`)(sequelize,Sequelize)
+db.Board = require(`./board`)(sequelize,Sequelize)
 
 
 Object.keys(db).forEach(modelName => {
