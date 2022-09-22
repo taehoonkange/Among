247b3e819ee43be30c8d60e3316bc1e2de73b728
taehoonkang <sls97117@gmail.com>
2022-09-22 19:59:03 +0900

    Feat: swagger-ui 추가

---
+++ packages/backend/swagger/swagger.js
@@ -0,0 +1,59 @@
+const swaggerAutogen = require("swagger-autogen")();
+
+const doc = {
+    info: {
+        title: "AMONG API",
+        description: "Description",
+    },
+    tags: [
+        {
+            name: 'User',
+            description: '유저 API',
+        },
+        {
+            name: 'Performance',
+            description: '공연 API',
+        },
+        {
+            name: 'Community',
+            description: '게시물 API',
+        },
+
+        {
+            name: 'TicketBook',
+            description: '티켓북 API',
+        }
+
+    ],
+    host: "localhost:3065",
+    schemes: ["http"],
+    definitions: {
+        Performance: {
+            title: "행사 제목 입력",
+            place: "행사 장소 입력",
+            price_infomation: "행사 가격 정보",
+            time: "행사 시간",
+            limitedAge: "제한된 나이",
+            term_start_at: "YYYY-MM-DD HH:MM:SS",
+            term_end_at: "YYYY-MM-DD HH:MM:SS",
+            start_at: "YYYY-MM-DD HH:MM:SS",
+            end_at: "YYYY-MM-DD HH:MM:SS",
+            description: "행사 상세 설명",
+            tickets: [{class: "좌석 등급", price: "좌석 가격", number: "좌석 번호", x: "x 좌표", y: "y 좌표", color: "색깔", status: "상태"},],
+            image: "이미지"
+        },
+
+        Post: {
+            content: "게시물 내용 입력",
+            image: ["이미지 주소 입력", "", ],
+            limitedReader: "VIP"
+        },
+    }
+};
+
+const outputFile = "./swagger-output.json";
+const endpointsFiles = ['./app.js']
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(async ()=>{
+    await import(`../app.js`)
+})
